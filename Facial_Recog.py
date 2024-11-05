3719039165ed044b82270f1add3a1c9d603dba07
Aidan Baydush <amb314@duke.edu>
2024-11-05 08:52:29 -0500

    changed requirements.txt

--- Facial_Recog.py
+++ Facial_Recog.py
@@ -1,3 +1,8 @@
 
+from facenet_pytorch import MTCNN, InceptionResnetV1
 
+# basing this model on this package with this link: https://github.com/timesler/facenet-pytorch
+
+
+# the goal being to adapt this to a raspberry pi system
 
