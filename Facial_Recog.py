d03cb0ac0a52a348d81d51a1b37e251dfac02a3a
Aidan Baydush <amb314@duke.edu>
2024-11-05 09:14:10 -0500

    added more imports and help() verbose string

--- Facial_Recog.py
+++ Facial_Recog.py
@@ -1,8 +1,17 @@
 
-from facenet_pytorch import MTCNN, InceptionResnetV1
+
 
 # basing this model on this package with this link: https://github.com/timesler/facenet-pytorch
-
+# basing on this model as well: https://www.kaggle.com/code/timesler/guide-to-mtcnn-in-facenet-pytorch
 
 # the goal being to adapt this to a raspberry pi system
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm.notebook import tqdm
+
+help(MTCNN)
+
+
 
