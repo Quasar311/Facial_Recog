7e213db6e840c458540d1e653659f30d28813a98
Aidan Baydush <amb314@duke.edu>
2024-11-14 08:53:22 -0500

    added 3 test images, working on understanding main model

--- Facial_Recog.py
+++ Facial_Recog.py
@@ -11,7 +11,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tqdm.notebook import tqdm
 
-help(MTCNN)
+
+model = MTCNN()
+
+
+mtcnn = MTCNN(image_size=<image_size>, margin=<margin>)
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+
+from PIL import Image
+
+path = 'C:\\Users\\Aidan\\Code\\tinkering\\Face_rec\\face_rec\\'
+
+img = Image.open(path)
+
+# Get cropped and prewhitened image tensor
+img_cropped = mtcnn(img, save_path=<optional save path>)
+
+# Calculate embedding (unsqueeze to add batch dimension)
+img_embedding = resnet(img_cropped.unsqueeze(0))
+
+# Or, if using for VGGFace2 classification
+resnet.classify = True
+img_probs = resnet(img_cropped.unsqueeze(0))
 
 
 
